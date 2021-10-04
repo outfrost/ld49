a341bace19e026c50b1f830de08fee85c1d1bfa9
Kiskadee <Kiskadee.dev@gmail.com>
2021-10-04 22:36:20 +0100

    Customer spawner increases the limit of customers every 20 seconds, after limit, decreases until 0

--- game/customer/spawner/CustomerSpawner.gd
+++ game/customer/spawner/CustomerSpawner.gd
@@ -21,11 +21,15 @@ var waiting:bool = false
 
 var game_is_running = true #Otherwise shut down the spawning, can be replaced for a more direct access
 
+var increasing_difficulty:bool = true
+var current_max_customers = 0
+var global_max_customers = 7 #Won't override the available seats
+
 func customer_despawning(node:Spatial):
 	instanced_customers.erase(node)
 
 func spawn_customer():
-	if not spawning_spots.empty() and has_free_seats():
+	if not spawning_spots.empty() and has_free_seats() and instanced_customers.size() < current_max_customers:
 		var spot = spawning_spots[randi() % spawning_spots.size()] #chooses random spawn spot based on the array
 		var customer_packed:PackedScene = customer_scenes[randi() % customer_scenes.size()]
 		var customer = customer_packed.instance()
@@ -60,3 +64,15 @@ func _process(delta):
 
 func _ready():
 	spawn_timer.start()
+
+
+func _on_RampDifficultyTimer_timeout():
+	if increasing_difficulty:
+		current_max_customers+=1
+		print("Increased limit of customers ", current_max_customers)
+		if current_max_customers > global_max_customers:
+			increasing_difficulty = false
+	else:
+		if current_max_customers > 0:
+			print("Decreased limit of customers ", current_max_customers)
+			current_max_customers-=1
