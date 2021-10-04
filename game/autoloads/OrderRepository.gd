1dd969975c173b79528b2aacd9b1343844dd50a9
Kiskadee <Kiskadee.dev@gmail.com>
2021-10-04 20:55:39 +0100

    Emits signal to clean the counter at appropriate time

--- game/autoloads/OrderRepository.gd
+++ game/autoloads/OrderRepository.gd
@@ -48,7 +48,6 @@ func barista_add_item_to_delivery(item:int)->void:
 
 #0 means garbage, #100 means excellent
 func compare_order(barista_order:Array, customer_order:Array)->int:
-	client_got_order_from_the_counter()
 	var barista_order_siz = barista_order.size()
 	var customer_order_size = customer_order.size()
 
@@ -62,6 +61,7 @@ func compare_order(barista_order:Array, customer_order:Array)->int:
 		if barista_order[i] != customer_order[i]:
 			missed_items += 1
 
+	client_got_order_from_the_counter()
 	if missed_items == customer_order_size:
 		print("All of the items are wrong")
 		return 0
