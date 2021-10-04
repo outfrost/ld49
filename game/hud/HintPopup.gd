e752db2eb87c56a744683c9adfc7e10220194957
outfrost <kotlet.bahn@gmail.com>
2021-10-04 22:27:15 +0200

    Implement hint queue

--- game/hud/HintPopup.gd
+++ game/hud/HintPopup.gd
@@ -7,24 +7,32 @@ onready var label: RichTextLabel = $Panel/RichTextLabel
 var time_shown: float = 0.0
 var duration: float = 0.0
 
+var queue = []
+
 func _ready() -> void:
 	hide()
 
 func _process(delta: float) -> void:
-	time_shown += delta
+	if !visible and !queue.empty():
+		var disp = queue.pop_front()
+		reset()
+		label.bbcode_text = disp.text
+		self.duration = disp.duration
+		show()
+
 	if time_shown > duration:
 		hide()
 		return
 	label.percent_visible = clamp(time_shown * TEXT_SHOW_RATE, 0.0, 1.0)
 
+	time_shown += delta
+
 func reset() -> void:
 	label.bbcode_text = ""
 	label.percent_visible = 0.0
 	time_shown = 0.0
+	queue.clear()
 	hide()
 
 func display(text: String, duration: float) -> void:
-	reset()
-	label.bbcode_text = text
-	self.duration = duration
-	show()
+	queue.push_back({ text = text, duration = duration })
