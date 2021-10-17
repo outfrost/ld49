e0d2b838674134111b564fb548b11011fdcb0c33
Ryan <raw9426@gmail.com>
2021-10-17 14:20:42 -0400

    Close issue #60
    Added material override to cups
    Adjusted Americano material override to be more distinct when next to expresso override material

--- game/cup/Cup.gd
+++ game/cup/Cup.gd
@@ -3,6 +3,16 @@ extends Spatial
 
 export(OrderRepository.possible_orders) var coffee_type
 
+var coffee_materials:Dictionary ={
+	OrderRepository.possible_orders.coffee_americano:load("res://assets/imports/ColorBand_Americano.tres"),
+	OrderRepository.possible_orders.coffee_cappuccino:load("res://assets/imports/ColorBand_Cappuccino.tres"),
+	OrderRepository.possible_orders.coffee_espresso:load("res://assets/imports/ColorBand_Espresso.tres"),
+	OrderRepository.possible_orders.coffee_latte:load("res://assets/imports/ColorBand_Latte.tres")
+}
+
 func _ready():
-	pass
+	if $paper_cup_ready_exportprep.has_node("PaperCup_Ready"):
+		$paper_cup_ready_exportprep/PaperCup_Ready.set_surface_material(1, coffee_materials[coffee_type])
+	else:
+		print("Model does not have expected node 'PaperCup_Ready'")
 
