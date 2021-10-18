b5f655e20793ad69bf000cedb0fe847c8e23efc9
Kiskadee <Kiskadee.dev@gmail.com>
2021-10-17 22:44:41 -0300

    Created a separate timer to manage the drinking time

--- game/customer/FSM/states/drinking.gd
+++ game/customer/FSM/states/drinking.gd
@@ -5,6 +5,8 @@ func enter():
 	.enter()
 	#Will change automatically to leaving after some time
 	base_customer.anim_state_machine.travel("customerDrinkIdle")
+	base_customer.max_waiting_timer.stop() #Uses the MaxDrinkingTime to adjust the customer drinking time to a different number
+	$MaxDrinkingTime.start() #<-------------------
 
 func exit():
 	.exit()
@@ -13,3 +15,7 @@ func _physics_process(delta):
 	if not active:
 		return
 	base_customer._face_focus_direction()
+
+
+func _on_MaxDrinkingTime_timeout():
+	base_customer._on_MaxWaitingTime_timeout()
